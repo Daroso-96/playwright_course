0dbd8ebcff5eb19b61d598ed43c789e0d0c48e89
Dayana Romero <daysoto96@gmail.com>
2023-09-02 15:36:58 -0500

    Test dos dando click a los botones

---
+++ test_dos.py
@@ -0,0 +1,12 @@
+import re
+from playwright.sync_api import Page, expect
+
+def test_dos(page: Page):
+    page.goto("https://demoqa.com/")
+    expect(page).to_have_title(("DEMOQA"))
+    #boton uno
+    boton_uno = page.locator("text =Elements")
+    #boton_uno = page.locator("text =Elements").click()
+    page.screenshot(path = "imagenes/boton_uno.png")
+    boton_uno.click()
+    page.screenshot(path = "imagenes/boton_uno_click.png")
