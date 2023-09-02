16afbbc8cf2c33dd5cbcea8712531f913da9c65d
Dayana Romero <daysoto96@gmail.com>
2023-09-02 16:22:36 -0500

    Se realizo modificaciónes y ahora podemos rellenar el campo nombre y email

--- test_dos.py
+++ test_dos.py
@@ -10,5 +10,16 @@ def test_dos(page: Page):
     page.screenshot(path = "imagenes/boton_uno.png")
     #boton_uno.click()
     #page.screenshot(path = "imagenes/boton_uno_click.png")
+    #Validar la url
+    expect(page).to_have_url(re.compile(".*elements"))
     page.locator("text=Text Box").click()
     page.screenshot(path='imagenes/boton_dos_click.png')
+    expect(page).to_have_url(re.compile(".*text-box"))
+
+    #Primer texto campo nombre
+    #page.locator("#userName").fill("Sasha")
+    page.locator("//input[@id = 'userName']").fill("Sasha")
+    page.screenshot(path='imagenes/texto_nombre.png')
+
+    page.locator("//input[@id = 'userEmail']").fill("sasha@dog.com")
+    page.screenshot(path='imagenes/texto_email.png')
