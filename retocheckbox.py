67546e4b9b60c25a9b2a7e7896380744d157befb
Dayana Romero <daysoto96@gmail.com>
2023-09-21 21:46:22 -0500

    Mejoras

--- retocheckbox.py
+++ retocheckbox.py
@@ -7,7 +7,7 @@ def run(playwright: Playwright) -> None:
     browser = playwright.chromium.launch(headless=False, slow_mo=2000)
     context = browser.new_context(
         viewport={'width':1500, 'height':800},
-        record_video_dir="Videos/reto/"
+        #record_video_dir="Videos/reto/"
     )
     page = context.new_page()
 
@@ -30,13 +30,48 @@ def run(playwright: Playwright) -> None:
 
     page.locator("//textarea[@id='wsf-1-field-33']").fill("Dirección dos permanente demo")
     #page.screenshot(path="Imagenes/texto_dirección2.png")
-
+     #Check manera 1
+    page.mouse.wheel(0,700)
+    time.sleep(1)
+    """"
     che1 = page.locator("//label[@id='wsf-1-label-36-row-2']")
     expect(che1).to_be_visible()
     che1.click()
     che2 = page.locator("//label[@id='wsf-1-label-36-row-3']")
     expect(che2).to_be_visible()
     che2.click()
+    """
+    #Manera dos
+    """""
+    che1 = page.locator("//label[@id='wsf-1-label-36-row-2']")
+    expect(che1).to_be_visible()
+    che1.check()
+    che2 = page.locator("//label[@id='wsf-1-label-36-row-3']")
+    expect(che2).to_be_visible()
+    che2.check()
+    che2 = page.locator("//label[@id='wsf-1-label-36-row-3']")
+    expect(che2).to_be_visible()
+    che2.uncheck()
+    assert che2.locator("//label[@id='wsf-1-label-36-row-3']").is_checked() is False
+     """
+    # page.locator("//label[contains(.,'PHP')]").click()
+    # page.locator("//label[contains(.,'PHP')]").click()
+    # time.sleep(1)
+    # page.locator("//label[contains(.,'JS')]").click()
+
+    # page.locator("//label[contains(.,'PHP')]").check()
+    # page.locator("//label[contains(.,'PHP')]").uncheck()
+    # time.sleep(2)
+    # page.locator("//label[contains(.,'JS')]").check()
+    # page.locator("//label[contains(.,'JS')]").uncheck()
+    # assert page.locator("//label[contains(.,'JS')]").is_checked() is False
+
+    # (//label[contains(@class,'wsf-label')])[7]
+
+    for i in range(7,9):
+        page.locator(f"(//label[contains(@class,'wsf-label')])[{i}]").click()
+        
+
     page.locator("//button[@id='wsf-1-field-34']").click()
 
     """
